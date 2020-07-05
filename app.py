b46c2f6c335acae3a9fa2a39a80c0b0b91aee2d8
sayannath <sayannath235@gmail.com>
2020-07-05 17:20:00 +0530

    Add

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ from flask import Flask, request
 from flask_restful import Resource, Api
 
 app = Flask(__name__)
+app.secret_key = 'sayan'
 api = Api(app)
 
 items = []
@@ -9,12 +10,12 @@ items = []
 
 class Item(Resource):
     def get(self, name):
-        for item in items:
-            if item['name'] == name:
-                return item
-        return {'item': "Item not found."}, 404
+        item = next(filter(lambda x: x['name'] == name, items), None)
+        return {'item': item}, 200 if item is not None else 404
 
     def post(self, name):
+        if next(filter(lambda x: x['name'] == name, items), None):
+            return {'message': "An item with name '{}' already exists.".format(name)}, 400    #400 is a bad request
         data = request.get_json()
         item = {'name': name, 'price': data['price']}
         items.append(item)
---
+++ security.py
@@ -0,0 +1,21 @@
+users = [
+    {
+        'id': 1,
+        'username': "sayan",
+        'password': "12345678"
+    }
+]
+
+username_mapping = {'sayan': {
+        'id': 1,
+        'username': "sayan",
+        'password': "12345678"
+    }
+}
+
+userid_mapping = {1: {
+        'id': 1,
+        'user': "sayan",
+        'password': "12345678"
+    }
+}
